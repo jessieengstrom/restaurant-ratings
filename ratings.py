e34ac73c250fb57382b7befce6bc977551dba8fd
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:12:48 -0800

    implemented user input validation

--- ratings.py
+++ ratings.py
@@ -1,10 +1,24 @@
 """Restaurant rating lister."""
 
+import sys
+
 
 # put your code here
 def create_restaurant_ratings(file_name):
 
-    restaurant_ratings = {}
+    restaurant = raw_input("What restaurant are you rating? > ")
+
+    while True:
+        try:
+            user_rating = int(raw_input("What is your rating? > "))
+            if user_rating < 1 or user_rating > 5:
+                print "Rating should be between 1 and 5"
+            else:
+                break
+        except ValueError:
+            print "Please enter an integer only"
+
+    restaurant_ratings = {restaurant: user_rating}
     with open(file_name) as my_file:
 
         for line in my_file:
@@ -16,9 +30,7 @@ def create_restaurant_ratings(file_name):
 
     new_list = sorted(restaurant_ratings.items())
     for rest, rat in new_list:
-        print rest + " is rated at " + rat + "."
+        print rest + " is rated at " + str(rat) + "."
 
 
-
-
-create_restaurant_ratings("scores.txt")
+create_restaurant_ratings(sys.argv[1])
