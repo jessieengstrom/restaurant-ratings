7deb1ec9638a25ed714473fca8e33925711ae67c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 16:17:05 -0800

    used with open

--- ratings.py
+++ ratings.py
@@ -5,25 +5,20 @@
 def create_restaurant_ratings(file_name):
 
     restaurant_ratings = {}
-    my_file = open(file_name)
+    with open(file_name) as my_file:
 
-    for line in my_file:
+        for line in my_file:
 
-        line = line.rstrip()
-        rating = line.split(":")
-        #print(rating)
-        restaurant_ratings[rating[0]] = rating[1]
+            line = line.rstrip()
+            rating = line.split(":")
+            #print(rating)
+            restaurant_ratings[rating[0]] = rating[1]
 
     new_list = sorted(restaurant_ratings.items())
     for rest, rat in new_list:
         print rest + " is rated at " + rat + "."
 
-    # sorted_ratings = {}
 
-    # for rest in new_list:
-    #     sorted_ratings[rest] = restaurant_ratings[rest]
-
-    # print sorted_ratings
 
 
 create_restaurant_ratings("scores.txt")
